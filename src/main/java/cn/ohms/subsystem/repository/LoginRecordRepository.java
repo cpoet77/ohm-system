c92102c617fa379fd409538564c311de985b6b50
whlstruggler <185419678@qq.com>
2020-05-22 13:22:28 +0800

    add LoginRecordRepository

--- src/main/java/cn/ohms/subsystem/repository/LoginRecordRepository.java
+++ src/main/java/cn/ohms/subsystem/repository/LoginRecordRepository.java
@@ -1,5 +1,6 @@
 package cn.ohms.subsystem.repository;
 
+import cn.ohms.subsystem.entity.LoginRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -7,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
  * @auther _Struggler
  * 2020/5/22 11:04
  */
-public interface LoginRecordRepository extends JpaRepository<LoginRecordRepository,String>, JpaSpecificationExecutor<LoginRecordRepository> {
+public interface LoginRecordRepository extends JpaRepository<LoginRecordEntity,String>, JpaSpecificationExecutor<LoginRecordEntity> {
 
 }
