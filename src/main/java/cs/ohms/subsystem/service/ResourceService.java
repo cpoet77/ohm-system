1abb5fe8c3a1e9c8b013aedb0a6d911879d362d4
nsleaf <nsleaf@foxmail.com>
2020-05-29 15:01:58 +0800

    添加文件上传功能

--- src/main/java/cs/ohms/subsystem/service/ResourceService.java
+++ src/main/java/cs/ohms/subsystem/service/ResourceService.java
@@ -1,6 +1,9 @@
 // The code file was created by <a href="https://www.nsleaf.cn">nsleaf</a> (email:nsleaf@foxmail.com) on 2020/05/22.
 package cs.ohms.subsystem.service;
 
+import cs.ohms.subsystem.common.ResponseResult;
+import cs.ohms.subsystem.entity.UserEntity;
+
 import java.io.InputStream;
 import java.util.List;
 
@@ -8,6 +11,16 @@ import java.util.List;
  * @author <a href="https://www.nsleaf.cn">nsleaf</a>
  */
 public interface ResourceService {
+    /**
+     * 普通文件存储路径
+     */
+    String commonFilePath = "upload/common";
+
+    /**
+     * 受保护文件存储路径
+     */
+    String confidentialFilePath = "upload/confidential";
+
     /**
      * 传入inputStream转换成table
      *
@@ -17,4 +30,10 @@ public interface ResourceService {
      * @return list
      */
     <T> List<T> inputStreamToTable(Class<T> clazz, InputStream in) throws Exception;
+
+    boolean isCommonFileFormat(String fix);
+
+    boolean isConfidentialFileFormat(String fix);
+
+    ResponseResult saveConfidentialResource(UserEntity attribute, String uuid, String name, String fix, String path, Boolean isPublic);
 }
--- src/main/java/cs/ohms/subsystem/service/impl/ResourceServiceImpl.java
+++ src/main/java/cs/ohms/subsystem/service/impl/ResourceServiceImpl.java
@@ -1,12 +1,20 @@
 // The code file was created by <a href="https://www.nsleaf.cn">nsleaf</a> (email:nsleaf@foxmail.com) on 2020/05/22.
 package cs.ohms.subsystem.service.impl;
 
-import cs.ohms.subsystem.service.ResourceService;
 import com.github.liaochong.myexcel.core.DefaultExcelReader;
+import cs.ohms.subsystem.common.ResponseResult;
+import cs.ohms.subsystem.entity.UserEntity;
+import cs.ohms.subsystem.service.AppService;
+import cs.ohms.subsystem.service.ResourceService;
+import cs.ohms.subsystem.utils.NStringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author <a href="https://www.nsleaf.cn">nsleaf</a>
@@ -14,8 +22,56 @@ import java.util.List;
  */
 @Service("resourceService")
 public class ResourceServiceImpl implements ResourceService {
+    /**
+     * 公共文件支持的后缀
+     */
+    private final static Set<String> COMMON_FILE_FORMAT_SET = new HashSet<>();
+
+    /**
+     * 受保护文件支持的后缀
+     */
+    private final static Set<String> CONFIDENTIAL_FILE_FORMAT_SET = new HashSet<>();
+
+    private AppService appService;
+
+    @Autowired
+    public ResourceServiceImpl(AppService appService) {
+        this.appService = appService;
+    }
+
+    @PostConstruct
+    public void init() {
+        String uploadCommonPattern = appService.getAsString("ohms.upload.common.pattern");
+        String uploadConfidentialPattern = appService.getAsString("ohms.upload.confidential.pattern");
+        if (!NStringUtil.isEmpty(uploadCommonPattern)) {
+            for (String pattern : uploadCommonPattern.split(",")) {
+                COMMON_FILE_FORMAT_SET.add(NStringUtil.joint(".{}", pattern));
+            }
+        }
+        if (!NStringUtil.isEmpty(uploadConfidentialPattern)) {
+            for (String pattern : uploadConfidentialPattern.split(",")) {
+                CONFIDENTIAL_FILE_FORMAT_SET.add(NStringUtil.joint(".{}", pattern));
+            }
+        }
+    }
+
     @Override
     public <T> List<T> inputStreamToTable(Class<T> clazz, InputStream in) throws Exception {
         return (DefaultExcelReader.of(clazz).sheet(0).rowFilter(row -> row.getRowNum() > 0).read(in));
+    }
+
+    @Override
+    public boolean isCommonFileFormat(String fix) {
+        return COMMON_FILE_FORMAT_SET.contains(fix);
+    }
+
+    @Override
+    public boolean isConfidentialFileFormat(String fix) {
+        return CONFIDENTIAL_FILE_FORMAT_SET.contains(fix);
+    }
+
+    @Override
+    public ResponseResult saveConfidentialResource(UserEntity attribute, String uuid, String name, String fix, String path, Boolean isPublic) {
+        return null;
     }
 }
