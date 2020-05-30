92f36e3d4780e8eac15389356aa0d0ee13d0c69a
nsleaf <nsleaf@foxmail.com>
2020-05-30 11:02:29 +0800

    基本完成所有数据表

---
+++ database/第一次迭代/Do Two/20200530.sql
@@ -0,0 +1,249 @@
+--
+-- 第一次迭代 Do Two 数据库sql语句
+-- nsleaf
+-- 2020.03.30
+--
+
+# DROP DATABASE IF EXISTS ohm_subsystem;
+
+CREATE DATABASE IF NOT EXISTS ohm_subsystem CHAR SET 'UTF8';
+
+USE ohm_subsystem;
+
+CREATE TABLE IF NOT EXISTS ohms_role
+(
+    id          TINYINT PRIMARY KEY AUTO_INCREMENT COMMENT '角色id',
+    name        NVARCHAR(25) NOT NULL COMMENT '用户名',
+    description NVARCHAR(64) NULL COMMENT '角色描述',
+    datetime    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加角色时间',
+    UNIQUE (name),
+    CHECK ( LENGTH(name) > 2 )
+) ENGINE = InnoDB COMMENT '用户角色表';
+
+CREATE TABLE IF NOT EXISTS ohms_user
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT COMMENT '用户id',
+    name      VARCHAR(48)  NOT NULL COMMENT '用户名',
+    real_name NVARCHAR(5)  NOT NULL COMMENT '姓名',
+    password  CHAR(32)     NOT NULL COMMENT '加密过的密码',
+    salt      CHAR(32)     NOT NULL COMMENT '加密用到的盐',
+    sex       CHAR(1)      NOT NULL DEFAULT 'M' COMMENT '用户性别',
+    avatar    VARCHAR(128) NULL COMMENT '用户头像地址',
+    email     VARCHAR(78)  NULL COMMENT '用户邮箱',
+    phone     VARCHAR(15)  NULL COMMENT '用户手机',
+    skin      VARCHAR(15)  NOT NULL DEFAULT 'green' COMMENT '界面皮肤',
+    UNIQUE (name),
+    CHECK ( LENGTH(name) > 3 ),
+    CHECK ( LENGTH(name) > 1 ),
+    CHECK ( LENGTH(password) = 32 ),
+    CHECK ( LENGTH(salt) = 32 ),
+    CHECK ( sex = 'M' OR sex = 'F' )
+) ENGINE = InnoDB COMMENT '用户表';
+
+CREATE TABLE IF NOT EXISTS ohms_user_role
+(
+    user_id  INT      NOT NULL COMMENT '用户id',
+    role_id  TINYINT  NOT NULL COMMENT '角色id',
+    datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '授于角色的时间',
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES ohms_user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES ohms_role (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB COMMENT '用户权限中间表';
+
+CREATE TABLE IF NOT EXISTS ohms_college
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT COMMENT '学院id',
+    name        NVARCHAR(64) NOT NULL COMMENT '学院名',
+    description TEXT         NULL COMMENT '学院描述',
+    datetime    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '导入时间',
+    UNIQUE (name),
+    CHECK ( LENGTH(name) > 2 )
+) ENGINE = InnoDB COMMENT '学院表';
+
+CREATE TABLE IF NOT EXISTS ohms_major
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT COMMENT '专业id',
+    name       NVARCHAR(64) NOT NULL COMMENT '专业名',
+    college_id INT          NOT NULL COMMENT '学院id',
+    datetime   DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '导入时间',
+    FOREIGN KEY (college_id) REFERENCES ohms_college (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (name),
+    CHECK ( LENGTH(name) > 4 )
+) ENGINE = InnoDB COMMENT '专业表';
+
+CREATE TABLE IF NOT EXISTS ohms_teacher
+(
+    teacher_id VARCHAR(24) PRIMARY KEY COMMENT '教职工号',
+    user_id    INT NOT NULL COMMENT '用户id',
+    FOREIGN KEY (user_id) REFERENCES ohms_user (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB COMMENT '老师表';
+
+CREATE TABLE IF NOT EXISTS ohms_course_group
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '课群id',
+    teacher_id  VARCHAR(24)   NOT NULL COMMENT '教职工号',
+    name        NVARCHAR(64)  NOT NULL COMMENT '课群名',
+    description NVARCHAR(255) NULL COMMENT '课群介绍',
+    create_time DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    state       BOOLEAN       NOT NULL DEFAULT TRUE COMMENT '课群状态',
+    FOREIGN KEY (teacher_id) REFERENCES ohms_teacher (teacher_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CHECK ( LENGTH(name) > 5 )
+) ENGINE = InnoDB COMMENT '课群表';
+
+CREATE TABLE IF NOT EXISTS ohms_student
+(
+    student_id CHAR(12) PRIMARY KEY COMMENT '学号',
+    user_id    INT NOT NULL COMMENT '用户id',
+    major_id   INT NOT NULL COMMENT '专业id',
+    FOREIGN KEY (user_id) REFERENCES ohms_user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (major_id) REFERENCES ohms_major (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CHECK ( LENGTH(student_id) = 12 )
+) ENGINE = InnoDB COMMENT '学生表';
+
+CREATE TABLE IF NOT EXISTS ohms_student_course_group
+(
+    student_id      VARCHAR(12) NOT NULL COMMENT '学号',
+    course_group_id BIGINT      NOT NULL COMMENT '课群id',
+    join_time       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '学生加入的时间',
+    PRIMARY KEY (student_id, course_group_id),
+    FOREIGN KEY (student_id) REFERENCES ohms_student (student_id),
+    FOREIGN KEY (course_group_id) REFERENCES ohms_course_group (id)
+) ENGINE = InnoDB COMMENT '学生加入的课群';
+
+CREATE TABLE IF NOT EXISTS ohms_login_record
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT COMMENT '登录记录id',
+    user_id       INT          NOT NULL COMMENT '用户id',
+    login_ip      VARCHAR(33)  NOT NULL COMMENT '登录的ip',
+    province      VARCHAR(30)  NULL COMMENT '登录的省',
+    province_code VARCHAR(6)   NULL COMMENT '省代码',
+    city          VARCHAR(50)  NULL COMMENT '登录的城市',
+    city_code     VARCHAR(6)   NULL COMMENT '城市代码',
+    address       VARCHAR(98)  NULL COMMENT '登录的地址',
+    agent         VARCHAR(255) NULL COMMENT 'user-agent',
+    datetime      DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登录时间',
+    FOREIGN KEY (user_id) REFERENCES ohms_user (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB COMMENT '用户登录记录';
+
+
+-- 创建站内消息记录表
+create table if not exists ohms_messagebox
+(
+    id        int auto_increment primary key comment '记录id',
+    title     nvarchar(45) not null comment '标题',
+    content   text         null comment '内容',
+    send_time datetime     not null default current_timestamp comment '发送时间',
+    check ( length(title) > 0)
+) engine = innodb comment '站内消息记录表';
+
+
+-- 创建站内消息中间表
+create table if not exists ohms_user_messagebox
+(
+    user_id    int     not null comment '用户id',
+    message_id int     not null comment '消息id',
+    is_read    boolean not null default false comment '是否已读',
+    primary key (user_id, message_id),
+    foreign key (user_id) references ohms_user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    foreign key (message_id) references ohms_messagebox (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB COMMENT '站内消息中间表';
+
+-- 创建资源记录表
+create table if not exists ohms_resource
+(
+    id        char(32) comment 'UUID',
+    user_id   int          null comment '上传用户id',
+    name      nvarchar(98) not null comment '资源名',
+    suffix    varchar(12)  not null comment '后缀',
+    datetime  datetime     not null default CURRENT_TIMESTAMP comment '上传时间',
+    publicity boolean      not null default false comment '是否公开',
+    path      varchar(128) not null comment '文件存储的path',
+    primary key (id),
+    check ( length(id) = 32 ),
+    foreign key (user_id) references ohms_user (id) on delete set null on update cascade
+) engine = innoDB comment '资源记录表';
+
+
+-- 创建作业表
+create table if not exists ohms_homework
+(
+    id              int comment '作业id',
+    course_group_id bigint comment '课群id',
+    title           nvarchar(64) not null comment '作业名',
+    content         text         null comment '内容',
+    start_time      datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP comment '开始时间',
+    end_time        datetime     NULL comment '截止时间',
+    state           BOOLEAN      NOT NULL DEFAULT TRUE COMMENT '是否已关闭',
+    primary key (id),
+    foreign key (course_group_id) references ohms_course_group (id) on delete cascade on update cascade
+) engine = innoDB comment '作业表';
+
+-- 创建学生已上传作业表
+create table if not exists ohms_push_homework
+(
+    id          int auto_increment primary key comment 'ID',
+    homework_id int      not null comment '作业id',
+    user_id     INT      not null comment '学生userId',
+    push_time   datetime not null default current_timestamp comment '上传时间',
+    text        text     null comment '作业描述',
+    foreign key (homework_id) references ohms_homework (id) on delete cascade on update cascade,
+    foreign key (user_id) references ohms_student (user_id) on delete cascade on update cascade
+) engine innoDB comment '学生已上传的作业';
+
+-- 创建作业资源中间表
+create table if not exists ohms_homework_resource
+(
+    homework_id int      not null comment '作业id',
+    resource_id char(32) not null comment '资源id',
+    primary key (homework_id, resource_id),
+    foreign key (homework_id) references ohms_homework (id) on delete cascade on update cascade,
+    foreign key (resource_id) references ohms_resource (id) on delete cascade on update cascade
+) engine innoDB comment '作业资源中间表';
+
+
+-- 创建已上传作业资源中间表
+create table if not exists ohms_push_homework_resource
+(
+    push_homework_id int      not null comment '作业id',
+    resource_id      char(32) not null comment '资源id',
+    primary key (push_homework_id, resource_id),
+    foreign key (push_homework_id) references ohms_push_homework (id) on delete cascade on update cascade,
+    foreign key (resource_id) references ohms_resource (id) on delete cascade on update cascade
+) engine = innoDB comment '已上传作业资源中间表';
+
+--
+-- 视图
+--
+
+-- 教师
+CREATE VIEW ohms_view_teacher AS
+SELECT u.*, t.teacher_id
+FROM ohms_user u,
+     ohms_teacher t
+WHERE t.user_id = u.id;
+
+-- 学生
+CREATE VIEW ohms_view_student AS
+SELECT u.*, s.student_id, s.major_id
+FROM ohms_user u,
+     ohms_student s
+WHERE u.id = s.user_id;
+
+--
+-- 初始化数据
+--
+INSERT INTO ohms_role(name, description)
+VALUES ('admin', '超级管理员'),
+       ('teachingSecretary', '教学秘书'),
+       ('teacher', '教师'),
+       ('student', '学生');
+
+INSERT INTO ohms_user(name, real_name, password, salt)
+VALUES ('gzmu-201742010122', '王国富', '3a9b59d90b4c3a62aa12bcb1a495de17', '48bcaffd039640b4aa63a86a6d0883da');
+
+INSERT INTO ohms_teacher(teacher_id, user_id)
+VALUES ('201742010122', 1);
+
+INSERT INTO ohms_user_role(user_id, role_id)
+VALUES (1, 1),
+       (1, 2);
