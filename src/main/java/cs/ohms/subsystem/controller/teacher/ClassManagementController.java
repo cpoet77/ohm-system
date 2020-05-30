44a06b0ecb598a8778bb102854730f8c685c3b60
nsleaf <nsleaf@foxmail.com>
2020-05-30 16:56:55 +0800

    添加Apache Commons Compress

--- src/main/java/cs/ohms/subsystem/controller/teacher/ClassManagementController.java
+++ src/main/java/cs/ohms/subsystem/controller/teacher/ClassManagementController.java
@@ -1,6 +1,7 @@
 // The code file was created by <a href="https://www.nsleaf.cn">nsleaf</a> (email:nsleaf@foxmail.com) on 2020/05/30.
 package cs.ohms.subsystem.controller.teacher;
 
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/teachingSecretary/classManagement")
+@RequiresAuthentication
 public class ClassManagementController {
     @GetMapping
     public String index(){
